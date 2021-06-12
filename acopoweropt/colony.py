7c2629ff3e215fd2c3d55800cfb59f1d64b733cb
Ettore Aquino <ettore@ettoreaquino.com>
2021-06-12 14:11:20 -0300

    Colony improved documentation

--- acopoweropt/colony.py
+++ acopoweropt/colony.py
@@ -1,20 +1,27 @@
-'''
-Singleton classes to initialize an Ant Colony
-'''
+"""
+Module to handle classes and methods related to the Ant Colony Optimizer
+"""
 
 import pandas as pd
 import random
 
 class Colony:
-    '''
-    Singleton Class to load a Colony.
-    
-    Attributes:
-        n_ants: Number of ants in the colony
-        food_srcs: Dataframe of possible food sources
-        phr_evp_rate: Pheromone evaporation rate
-    '''
+    """Singleton Class to load an Ant Colony.
 
+    Parameters
+    ----------
+    n_ants : int
+        Number of ants in the colony
+    phr_evp_rate : float
+        Pheromone evaporation rate
+
+    Attributes
+    ----------
+    n_ants : int
+        Number of ants in the colony
+    phr_evp_rate : float
+        Pheromone evaporation rate
+    """
     def __init__(self, n_ants: int, phr_evp_rate: float):
 
         self.n_ants = n_ants
