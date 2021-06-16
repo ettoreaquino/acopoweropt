0fe387c50fa2c8667b6e0b2f210bb93033310dc6
Ettore Aquino <ettore@ettoreaquino.com>
2021-06-16 01:21:14 -0300

    Improved tests for initialization

--- tests/test_colony.py
+++ tests/test_colony.py
@@ -7,10 +7,24 @@ from acopoweropt import system
 class TestColony(TestCase):
     def test_PowerColony_should_correctly_initialize(self):
         with self.subTest():
-            System = system.PowerSystem(name="s15")
+            System = system.PowerSystem(name="s10")
+            n_ants = 5
+            pheromone_evap_rate = 0.25
             Colony = colony.PowerColony(
-                n_ants=5, pheromone_evap_rate=0.25, PowerSystem=System
+                n_ants=n_ants,
+                pheromone_evap_rate=pheromone_evap_rate,
+                PowerSystem=System,
             )
 
-            self.assertEqual(Colony.n_ants, 5)
-            self.assertEqual(Colony.phr_evp_rate, 0.25)
+            self.assertEqual(Colony.n_ants, n_ants)
+            self.assertEqual(Colony.phr_evp_rate, pheromone_evap_rate)
+
+            # Initial paths should have the same n of rows as the number of ants
+            self.assertTrue(len(Colony.initial_paths.index) == n_ants)
+
+            # Pheromone matrix should have a shape of TGUs X Maximum number of operative
+            # zones * 2 (extra columns for probabilites of each zone)
+            self.assertTrue(
+                Colony.pheromone.shape
+                == (System.opzs.index.max(), System.opzs.max() * 2)
+            )
